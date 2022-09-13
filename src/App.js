2e15ae5e8b607b025a7b2d34ee79a3543ddfbb13
Patrick Dantas <patrickdsilva99@gmail.com>
2022-09-13 16:11:50 -0400

    expense item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenseData = [
+    {
+      date: new Date(),
+      title: 'Buceta',
+      amount: 1000.00,
+    },    
+    {
+      date: new Date(),
+      title: 'Cacete',
+      amount: 2000.00,
+    }
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      {expenseData.map((ex) => (
+        <ExpenseItem title={ex.title} date={ex.date} amount={ex.amount}></ExpenseItem>
+      ))}
     </div>
   );
 }
