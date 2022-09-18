4178e608bfd75b51fe390685594af7b02aeee3be
Patrick Dantas <patrickdsilva99@gmail.com>
2022-09-17 20:53:22 -0400

    id problem solved

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const today = new Date();
 const initialExpenseData = [
   {
-    title: 'Title1',
-    amount: 1000.50,
-    date: today,    
-  },    
+    title: "Title1",
+    amount: 1000.5,
+    date: today,
+  },
   {
-    title: 'Title2',
-    amount: 2000.00,
-    date: today,  
-  },  
+    title: "Title2",
+    amount: 2000.0,
+    date: today,
+  },
   {
-    title: 'Title3',
-    amount: 3000.00,
-    date: today,  
-  },    
+    title: "Title3",
+    amount: 3000.0,
+    date: today,
+  },
   {
-    title: 'Title4',
-    amount: 4000.00,
-    date: today,  
-  }
+    title: "Title4",
+    amount: 4000.0,
+    date: today,
+  },
 ];
 
-initialExpenseData.map((exp,id) => {
+initialExpenseData.map((exp, id) => {
   exp.id = id;
-})
+});
 
 function App() {
-
-  const [expenseData,setExpenseData] = useState(initialExpenseData);
-  const [removeExpenseActive,setRemoveExpenseActive] = useState(false);
+  const [expenseData, setExpenseData] = useState(initialExpenseData);
+  const [removeExpenseActive, setRemoveExpenseActive] = useState(false);
 
   const addExpenseHandler = (newExpense) => {
     setExpenseData((prevExpenses) => {
-      newExpense.id = prevExpenses.length;
-      return [newExpense,...prevExpenses];
+      let biggest = 0;
+      prevExpenses.forEach((ex) => {
+        if(ex.id > biggest) biggest = ex.id;
+      })
+      newExpense.id = ++biggest;
+      return [newExpense, ...prevExpenses];
     });
-  }
+  };
 
   const removeExpenseHandler = (expense) => {
     const removeExpense = expenseData.filter(
@@ -48,15 +51,22 @@ function App() {
     );
     setExpenseData(removeExpense);
   };
-  
+
   const removeExpenseButtonActiveHandler = (isActive) => {
     setRemoveExpenseActive(isActive);
-  }
-  
+  };
+
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} onRemoveExpenseActive={removeExpenseButtonActiveHandler}/>
-      <Expenses items={expenseData} removeExpenseActive={removeExpenseActive} removeExpenseHandler={removeExpenseHandler}/>
+      <NewExpense
+        onAddExpense={addExpenseHandler}
+        onRemoveExpenseActive={removeExpenseButtonActiveHandler}
+      />
+      <Expenses
+        items={expenseData}
+        removeExpenseActive={removeExpenseActive}
+        removeExpenseHandler={removeExpenseHandler}
+      />
     </div>
   );
 }
